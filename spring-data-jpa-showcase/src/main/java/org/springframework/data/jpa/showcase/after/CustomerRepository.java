7b1575346eeb8abd7ef4c9bef7c23901c126e86c
Oliver Gierke <info@olivergierke.de>
2011-02-15 07:26:51 +0100

    Removed obsolete method from CustomerRepository.

--- spring-data-jpa-showcase/src/main/java/org/springframework/data/jpa/showcase/after/CustomerRepository.java
+++ spring-data-jpa-showcase/src/main/java/org/springframework/data/jpa/showcase/after/CustomerRepository.java
@@ -1,7 +1,5 @@
 package org.springframework.data.jpa.showcase.after;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,15 +12,6 @@ import org.springframework.data.jpa.showcase.core.Customer;
  * @author Oliver Gierke
  */
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-    /**
-     * Returns all {@link Customer}s with the given lastname.
-     * 
-     * @param lastname
-     * @return
-     */
-    List<Customer> findByLastname(String lastname);
-
 
     /**
      * Returns a page of {@link Customer}s with the given lastname.
